1184c5a24d36d6e7f720e482b8831f84d6dd50dc
Edem <augustine.akato@amalitech.com>
2025-08-13 15:08:51 +0000

    initial setup

---
+++ services/discovery-service/src/test/java/com/finedine/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.finedine.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
