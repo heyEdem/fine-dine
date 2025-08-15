b1c01008f5b755441926dbf4b33de33ffbbe9609
heyEdem <akatoaugustine@gmail.com>
2025-08-15 15:33:44 +0000

    further setup

---
+++ services/rider-service/src/test/java/com/finedine/riderservice/RiderServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.finedine.riderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class RiderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
